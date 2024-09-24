c471b4c02c202cd87165d1494519bf000df1624c
KRCautomotive <krycholrc@tlen.pl>
2024-09-24 09:05:55 +0200

    Update board.mk

--- board.mk
+++ board.mk
@@ -1,15 +1,17 @@
-BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp \
-    $(BOARD_DIR)/default_tune.cpp \
-
-
-BOARDINC += $(BOARD_DIR)/generated/controllers/generated
+# List of all the board related files.
+BOARDCPPSRC =  $(BOARD_DIR)/board_configuration.cpp
 
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
+# Override DEFAULT_ENGINE_TYPE
+DDEFS += -DSTM32F407xx
+DDEFS += -DFIRMWARE_ID=\"KRC_M52\"
+DDEFS += -DHAL_TRIGGER_USE_PAL=TRUE
+
 # reduce memory usage monitoring
 DDEFS += -DRAM_UNUSED_SIZE=100
 
-# assign critical LED to a non-existent pin if you do not have it on your board
-# good old PD14 is still the default value
-# DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::I15
+# Not use critical LED
+DDEFS += -DLED_CRITICAL_ERROR_BRAIN_PIN=Gpio::Unassigned
+
