3c714ab2407ff096325c272f8bd39d961e31b2bf
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2024-09-24 09:19:33 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -12,52 +12,59 @@ Gpio getWarningLedPin() {
 
 void setBoardDefaultConfiguration() {
 
-	//Injector output
-	engineConfiguration->injectionPinMode = OM_DEFAULT;
-	engineConfiguration->injectionPins[0] = Gpio::C6;
-	engineConfiguration->injectionPins[1] = Gpio::D10;
-	engineConfiguration->injectionPins[2] = Gpio::D14;
-	engineConfiguration->injectionPins[3] = Gpio::D12;
-	engineConfiguration->injectionPins[4] = Gpio::D15;
-	engineConfiguration->injectionPins[5] = Gpio::D13;
-
-        //Ignition output
-	engineConfiguration->ignitionPinMode = OM_DEFAULT;
-	engineConfiguration->ignitionPins[0] = Gpio::E2;
-	engineConfiguration->ignitionPins[1] = Gpio::E3;
-	engineConfiguration->ignitionPins[2] = Gpio::C13;
-	engineConfiguration->ignitionPins[3] = Gpio::E6;
-	engineConfiguration->ignitionPins[4] = Gpio::E4;
-	engineConfiguration->ignitionPins[5] = Gpio::E5;
-
 	//Digital out
-	engineConfiguration->fuelPumpPin = Gpio::E11;
+	engineConfiguration->boostControlPin = Gpio::A8;
+	engineConfiguration->fanPin = Gpio::B7;
+	engineConfiguration->mainRelayPin = Gpio::C13;
 	engineConfiguration->tachOutputPin = Gpio::D7;
+	engineConfiguration->fuelPumpPin = Gpio::E11;
 
-	//Analog
-	engineConfiguration->iat.adcChannel = EFI_ADC_0;
-	engineConfiguration->clt.adcChannel = EFI_ADC_1;
-	engineConfiguration->tps1_1AdcChannel = EFI_ADC_2;
-	engineConfiguration->vbattAdcChannel = EFI_ADC_4;
-	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-	engineConfiguration->afr.hwChannel = EFI_ADC_8;
-
-	engineConfiguration->map.sensor.type = MT_MPXH6400;
-
-	//Trigger 
+	//Input pin
+	engineConfiguration->vehicleSpeedSensorInputPin = Gpio::B6;
 	engineConfiguration->triggerInputPins[0] = Gpio::D3;
 	engineConfiguration->camInputs[0] = Gpio::D4;
 
+	// Idle configuration
+        engineConfiguration->useStepperIdle = false;
+        engineConfiguration->isDoubleSolenoidIdle = true;
+        engineConfiguration->idle.solenoidPin = Gpio::B9;
+        engineConfiguration->secondSolenoidPin = Gpio::B8;
+
+	//Analog
+	engineConfiguration->clt.adcChannel = EFI_ADC_0;
+	engineConfiguration->iat.adcChannel = EFI_ADC_1;
+	engineConfiguration->tps1_1AdcChannel = EFI_ADC_6;
+	engineConfiguration->vbattAdcChannel = EFI_ADC_4;
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
+
+	engineConfiguration->map.sensor.type = MT_MPXH6400;
+	engineConfiguration->baroSensor.type = MT_MPXH6400;
+
 	//Bias resistor
-	engineConfiguration->clt.config.bias_resistor = 2490;
-	engineConfiguration->iat.config.bias_resistor = 2490;
+	engineConfiguration->clt.config.bias_resistor = 2200;
+	engineConfiguration->iat.config.bias_resistor = 2200;
 
 	//Analog calc
-        engineConfiguration->analogInputDividerCoefficient = 1.52f;
-        engineConfiguration->vbattDividerCoeff = (7.55f / 1.0f);
-        engineConfiguration->adcVcc = 3.24f;
+        engineConfiguration->analogInputDividerCoefficient = 1.55f;
+        engineConfiguration->vbattDividerCoeff = (7.47f / 1.0f);
+        engineConfiguration->adcVcc = 3.12f;
 
-        //SD card
+	// SPI1 AUX
+	engineConfiguration->is_enabled_spi_1 = true;
+	engineConfiguration->max31855spiDevice = SPI_DEVICE_1;
+	engineConfiguration->spi1mosiPin = Gpio::B5;
+	engineConfiguration->spi1misoPin = Gpio::B4;
+	engineConfiguration->spi1sckPin = Gpio::B3;
+
+	// SPI2 EGT
+	engineConfiguration->is_enabled_spi_2 = true;
+	engineConfiguration->max31855spiDevice = SPI_DEVICE_2;
+	engineConfiguration->spi1mosiPin = Gpio::Unassigned;
+	engineConfiguration->spi1misoPin = Gpio::B14;
+	engineConfiguration->spi1sckPin = Gpio::B13;
+	engineConfiguration->max31855_cs[0] = Gpio::B12;
+
+        //SPI3 SD card
 	engineConfiguration->isSdCardEnabled = true;
 	engineConfiguration->is_enabled_spi_3 = true;
 	engineConfiguration->sdCardSpiDevice = SPI_DEVICE_3;
@@ -76,10 +83,8 @@ void setBoardDefaultConfiguration() {
 	engineConfiguration->canBaudRate = B500KBPS;
 
 	//Engine configuration
-	engineConfiguration->cylindersCount = 6;
-	engineConfiguration->firingOrder = FO_1_5_3_6_2_4;
 	engineConfiguration->ignitionMode = IM_INDIVIDUAL_COILS;
-	engineConfiguration->crankingInjectionMode = IM_SIMULTANEOUS;
+	engineConfiguration->crankingInjectionMode = IM_SEQUENTIAL;
 	engineConfiguration->injectionMode = IM_SEQUENTIAL;
 	
 
