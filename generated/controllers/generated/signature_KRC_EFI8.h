ad7e0205be2955952ccc8e4befc808b272694a89
GitHub git update Action <action@github.com>
2024-09-24 07:31:33 +0000

    Custom Auto-generated Configs
    [no ci]

---
+++ generated/controllers/generated/signature_KRC_EFI8.h
@@ -0,0 +1,6 @@
+//
+// was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
+//
+
+#define SIGNATURE_HASH 1400551136
+#define TS_SIGNATURE "rusEFI main.2024.09.24.KRC_EFI8.1400551136"
