45ae3212e0aa57a3502ba690c0541481022bc848
GitHub git update Action <action@github.com>
2024-09-25 01:25:10 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KRC_EFI8.h
+++ generated/controllers/generated/rusefi_generated_KRC_EFI8.h
@@ -1293,7 +1293,7 @@
 #define show_Proteus_presets false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 1400551136
+#define SIGNATURE_HASH 452860376
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1750,7 +1750,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.09.24.KRC_EFI8.1400551136"
+#define TS_SIGNATURE "rusEFI main.2024.09.25.KRC_EFI8.452860376"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
