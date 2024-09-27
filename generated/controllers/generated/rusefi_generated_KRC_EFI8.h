cb05a8316f84c363e69ebc47cd4d6a65daaa9b8f
GitHub git update Action <action@github.com>
2024-09-27 09:45:01 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KRC_EFI8.h
+++ generated/controllers/generated/rusefi_generated_KRC_EFI8.h
@@ -1293,7 +1293,7 @@
 #define show_Proteus_presets false
 #define show_test_presets true
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 3028909308
+#define SIGNATURE_HASH 2844377454
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -1610,7 +1610,7 @@
 #define ts_show_cam2 true
 #define ts_show_can2 false
 #define ts_show_can_bitrate true
-#define ts_show_can_pins true
+#define ts_show_can_pins false
 #define ts_show_can_wbo true
 #define ts_show_charge_estimation true
 #define ts_show_check_engine true
@@ -1704,7 +1704,7 @@
 #define ts_show_reset_calibrations false
 #define ts_show_rotary false
 #define ts_show_sd_card true
-#define ts_show_sd_pins true
+#define ts_show_sd_pins false
 #define ts_show_second_bank true
 #define ts_show_software_knock true
 #define ts_show_speedo_settings true
@@ -1751,7 +1751,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2024.09.27.KRC_EFI8.3028909308"
+#define TS_SIGNATURE "rusEFI main.2024.09.27.KRC_EFI8.2844377454"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_SINGLE_WRITE_COMMAND 'W'
--- generated/controllers/generated/signature_KRC_EFI8.h
+++ generated/controllers/generated/signature_KRC_EFI8.h
@@ -2,5 +2,5 @@
 // was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 3028909308
-#define TS_SIGNATURE "rusEFI main.2024.09.27.KRC_EFI8.3028909308"
+#define SIGNATURE_HASH 2844377454
+#define TS_SIGNATURE "rusEFI main.2024.09.27.KRC_EFI8.2844377454"
