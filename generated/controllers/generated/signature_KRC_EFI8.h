9ca27096f90d27bbd6869fa46122c7917b876dd1
Krycholrc <78201348+Krycholrc@users.noreply.github.com>
2024-10-04 09:23:22 +0200

    Delete generated/controllers/generated/signature_KRC_EFI8.h

--- generated/controllers/generated/signature_KRC_EFI8.h
+++
@@ -1,6 +0,0 @@
-//
-// was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
-//
-
-#define SIGNATURE_HASH 1039401507
-#define TS_SIGNATURE "rusEFI main.2024.10.04.KRC_EFI8.1039401507"
